48853d4805fea5e01eea285af7e2c1e2dbf8ecfb
Andrii <greedyguts@gmail.com>
2021-02-22 12:25:25 +0200

    02Components

---
+++ socialnetwork/src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile =()=> {
+    return ( <div className='content'>
+            <div>
+                <img src='https://i.pinimg.com/originals/4d/d5/e1/4dd5e17d9980833cabbc3f25e29181b5.jpg'/>
+            </div>
+            <div>
+                Ava + description
+            </div>
+            <div>
+                My Posts
+                <div>
+                    New Post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Profile;
